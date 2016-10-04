d5bcfa5955cebb9b61d7d4ae0bfe389cb54035dd
Izaak Weiss <izwe5504@colorado.edu>
2016-10-04 10:08:12 -0600

    Finished Diffie Hellman exchange using Elliptic Curves

--- Field.hs
+++ Field.hs
@@ -1,38 +1,47 @@
-{-# LANGUAGE TypeInType, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE TypeInType, ScopedTypeVariables, TypeApplications, DataKinds #-}
 
 module Field where
 
-    import Data.Maybe
     import Data.Proxy
-    import Data.Ratio
     import GHC.TypeLits
 
-
-
+    -- This is the extended greatest common demominator algorithm
     gcdExt :: Integer -> Integer -> (Integer, Integer, Integer)
     gcdExt a 0 = (1, 0, a)
     gcdExt a b = (t, s - q * t, g)
         where (q, r) = quotRem a b
               (s, t, g) = gcdExt b r
 
+    -- This function returns the multiplicative inverse (1/a) of a number
+    -- in the integers modulo n. If n is prime, then 1/a is guaranteed to be
+    -- another integer modulus n unless a == 0.
     modMulInv :: Integer -> Integer -> Integer
     modMulInv a n = i
         where (i, _, _) = gcdExt a n
 
+    -- This function checks every odd number and two by calling itself until
+    -- the number being checked is greater than n / 2.
+    -- TODO: This should be optimized into a check for the sqrt of n, not n / 2
     isPrimeLoop :: Integer -> Integer -> Bool
     isPrimeLoop n i
-        | i == n = True
+        | i > (div n 2) = True
         | i == 2 = (mod n i /= 0) && isPrimeLoop n (i+1)
         | otherwise = (mod n i /= 0) && isPrimeLoop n (i+2)
 
+    -- This function calls isPrimeLoop with the initial value of 2.
     isPrime :: Integer -> Bool
     isPrime n = isPrimeLoop n 2
 
+    -- This defined a new type FieldElem
+    -- an element of a prime field Z/n where n is prime
     newtype FieldElem (n :: Nat) = FieldElem Integer
 
+    -- This takes a FieldElem and makes it into an Integer
     toInt :: forall n. KnownNat n => FieldElem n -> Integer
     toInt (FieldElem x) = x
 
+    -- This subclasses the Num type, giving FieldElem addition, subtraction
+    -- multiplucation, conversion from an integer, absolute value, and sign
     instance KnownNat n => Num (FieldElem n) where
         FieldElem x + FieldElem y = FieldElem (mod (x + y) n)
             where
@@ -49,20 +58,20 @@ module Field where
         abs x = x
         signum x = 1
 
+    -- This provides a way for FieldElems to be printed easily.
     instance KnownNat n => Show (FieldElem n) where
-        show (FieldElem x) = show x ++ "(mod " ++ show n ++ ")"
+        show (FieldElem x) = show x ++ " mod " ++ show n
             where
                 n = natVal (Proxy :: Proxy n)
 
-    instance KnownNat n => Read (FieldElem n) where
-        readsPrec _ input = [(FieldElem x,"")]
-            where
-                xstr = takeWhile (/= '(') input
-                x = read xstr :: Integer
-
+    -- This provides a way for FieldElems to be compared for equality.
     instance KnownNat n => Eq (FieldElem n) where
         FieldElem x == FieldElem y = x == y
 
+    -- Since division isn't part of the Num class, we create a new Field division
+    -- operator using modMulInv
+    -- TODO: What's the most useful way to deal with division by zero errors in
+    -- Haskell?
     (//) :: forall n. KnownNat n => FieldElem n -> FieldElem n -> FieldElem n
     (//) (FieldElem x) (FieldElem y)
         | y == 0 = error "Division by Zero"
--- elliptic.hs
+++ elliptic.hs
@@ -1,49 +1,58 @@
 {-# LANGUAGE TypeInType, ScopedTypeVariables, TypeApplications, DataKinds #-}
 
 import Data.Bits
-import Data.Ratio
-import Data.Proxy
-import Data.Maybe
-import GHC.TypeLits
+import Data.Char
 
 import Field
 
+-- Create a type and constructor so we don't keep having to declare what
+-- size field this is, especially as the prime gets huge
 type BigField = FieldElem 3851
 
 bigField :: Integer -> FieldElem 3851
 bigField x = FieldElem x
 
+-- This is the type that holds the parameters for the elliptic curve itself
 data Curve = Curve {alpha::BigField, beta::BigField} deriving (Eq, Show)
 
+-- This function provides a formatted form for the
 formatCurve :: Curve -> String
-formatCurve x = "y^2 = x^3 + (" ++ show (alpha x) ++ ")x + (" ++ show (beta x) ++ ")"
+formatCurve x = "y^2 = x^3 + (" ++ show (alpha x) ++ ")x + ("
+    ++ show (beta x) ++ ")"
 
-data Point = Point {xcoord::BigField, ycoord::BigField} | Ideal deriving (Eq, Show, Read)
-data Line = Line {point1::Point, point2::Point}
+-- This is the type that holds a point on the curve
+data Point = Point {xcoord::BigField, ycoord::BigField} | Ideal
+    deriving (Eq, Show)
 
+-- This function tests to see if a Point is on the Curve
 testPoint :: Curve -> Point -> Bool
 testPoint (Curve a b) Ideal = True
-testPoint (Curve a b) (Point x y) = (y*y == (x*x*x + a*x + b))
+testPoint (Curve a b) (Point x y) = (y^2 == (x^3 + a*x + b))
 
+-- This function takes a point and negates it
 negPoint :: Point -> Point
 negPoint (Point x y) = Point x (-y)
 
+-- This function takes two points and "adds" them, assuming they are different,
+-- non ideal points
 simpleAdd :: Curve -> Point -> Point -> Point
 simpleAdd c p q = Point x (-y)
     where
         m = (ycoord q - ycoord p) // (xcoord q - xcoord p)
-        x = m*m - xcoord q - xcoord p
+        x = m^2 - xcoord q - xcoord p
         y = m * (x - xcoord p) + ycoord p
 
+-- This function "adds" two points that happen to be the same point
 derivAdd :: Curve -> Point -> Point
 derivAdd c p
     | ycoord p == 0 = Ideal
     | otherwise = Point x (-y)
     where
-        m = (3 * (xcoord p)*(xcoord p) + alpha c) // (2 * ycoord p)
+        m = (3 * (xcoord p)^2 + alpha c) // (2 * ycoord p)
         x = m*m - 2 * xcoord p
         y = m * (x - xcoord p) + ycoord p
 
+-- This function uses the above to add points in various cases
 addPoint :: Curve -> Point -> Point -> Point
 addPoint _ x Ideal = x
 addPoint _ Ideal x = x
@@ -52,6 +61,8 @@ addPoint c p q
     | xcoord p == xcoord q = Ideal
     | otherwise = simpleAdd c p q
 
+-- This function implements the loop for multiplication that is more efficient
+-- than repeated addition
 mulLoop :: Curve -> Integer -> Integer -> Point -> Point -> Point
 mulLoop curve index limit acc obj
     | index > limit = acc
@@ -62,6 +73,7 @@ mulLoop curve index limit acc obj
         r = (addPoint curve acc q)
         j = (shift index 1)
 
+-- This function uses the above to multiply a point times and integer
 mulPoint :: Curve -> Integer -> Point -> Point
 mulPoint _ _ Ideal = Ideal
 mulPoint c n p
@@ -71,16 +83,38 @@ mulPoint c n p
     | mod n 2 == 1 = mulLoop c 2 n p p
     | otherwise = mulLoop c 2 n Ideal p
 
+-- This function parses a point out of a string representation of a pair of
+-- integers, separated by soem string of non-digits.
+parsePoint :: [Char] -> Point
+parsePoint str = Point (bigField pint) (bigField qint)
+    where
+        (_,str1) = break isDigit str
+        (p,str2) = span isDigit str1
+        (_,str3) = break isDigit str2
+        (q,_) = span isDigit str3
+        pint = (read p :: Integer)
+        qint = (read q :: Integer)
+
+-- This function reduces a point to a pair of Integers
+pointPair :: Point -> (Integer,Integer)
+pointPair (Point a b) = (toInt a, toInt b)
+
+-- This is the main function which asks for user input
 main = do
     putStrLn "## Testing Curves ##"
     let curve = Curve (bigField 324) (bigField 1287)
     let cmul = mulPoint curve
-    putStrLn ("curve = " ++ formatCurve curve)
+    --uncomment below to view the elliptic curve we are working on
+    --putStrLn $ "curve = " ++ formatCurve curve
     let p = Point (bigField 920) (bigField 303)
-    putStrLn ("p = " ++ show p)
-    putStrLn ("Enter your secret number")
-    my_secret <- getLine
-    putStrLn ("Send this to your partner: " ++ show (cmul (read my_secret ::Integer) p))
-    putStrLn "Put your partner's point here"
+    --uncomment below to view the point we start with
+    --putStrLn $ "p = " ++ (show $ pointPair p)
+    putStrLn "Enter your secret number"
+    my_str <- getLine
+    let my_secret = read my_str :: Integer
+    putStrLn $ "Send this to your partner: "
+        ++ (show $ pointPair (cmul my_secret p))
+    putStrLn "Put your partner's point here: "
     partner_point <- getLine
-    putStrLn ("Final Shared Secret (Keep Secret) = " ++ show (cmul (read my_secret ::Integer) (read partner_point :: Point) ) )
+    putStrLn $ "Final Shared Secret (Keep Secret) = "
+        ++ (show $ pointPair (cmul my_secret (parsePoint partner_point)))
